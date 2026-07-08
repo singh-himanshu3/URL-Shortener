30e9f6406ea482f041ab97fc56ad76d1d6cb2442
singh-himanshu3 <himanshulautnar@gmail.com>
2026-07-08 14:53:08 +0000

    Add Procfile and environment variable support for deployment

--- database.py
+++ database.py
@@ -1,7 +1,11 @@
 import psycopg2
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 def get_db():
-    conn = psycopg2.connect("dbname=postgres user=postgres password=postgres123 host=localhost")
+    conn = psycopg2.connect(os.environ.get("DATABASE_URL", "dbname=postgres user=postgres password=postgres123 host=localhost"))
     try:
         yield conn
     finally:
